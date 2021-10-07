666cb79875a8f13f079017a9f9c648ed269e92a3
yanis khellaf <yanis.khellaf@outlook.fr>
2021-10-07 19:08:03 +0200

    menu collant, et element collant sur une partie du scroll ok

--- fixed.js
+++ fixed.js
@@ -9,55 +9,75 @@
     const scrollY = () => {
         var supportPageOffset = window.pageXOffset !== undefined;
         var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
-
         return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
     }
 
-    const elements = document.querySelectorAll('[data-fixed]')
-    elements.forEach(element => {
-        //Variables  
-    let rect = element.getBoundingClientRect()
-    const offset = parseInt(element.getAttribute('data-offset') || 0, 10)
-    if (!offset) {
-
-    }
-    let top = rect.top + scrollY()
-    const fake = document.createElement('div')
-    fake.style.width = rect.width + 'px'
-    fake.style.height = rect.height + 'px'
+    //window.function rend la fonction globale
+    window.makeStiky = (element) => {
+        //Variables 
+        let rect = element.getBoundingClientRect()
+        const offset = parseInt(element.getAttribute('data-offset') || 0, 10)
     
-    //Fonctions
-
-        const onScroll = function() {
-            const hasScrollClass = element.classList.contains('fixed')
-            if (scrollY() > top - offset && !hasScrollClass) {
-                element.classList.add('fixed')
-                element.style.top = offset + 'px'
-                element.style.width = rect.width + 'px'
-                element.parentNode.insertBefore(fake, element)
-            } else if (scrollY() < top - offset && hasScrollClass) {
-                element.classList.remove('fixed')
-                element.parentNode.removeChild(fake)
-
+        // if (element.getAttribute('data-constraint')) {
+        //     let constraint = document.querySelector(element.getAttribute('data-constraint'))        
+        // } else {
+        //     let constraint = document.body
+        // }
+        let constraint = (element.getAttribute('data-constraint')) ? document.querySelector(element.getAttribute('data-constraint')) : document.body
+        let constraintRect = constraint.getBoundingClientRect()
+        let constraintBottom = constraintRect.top + scrollY() + constraintRect.height - offset - rect.height
+        let top = rect.top + scrollY()
+        const fake = document.createElement('div')
+        fake.style.width = rect.width + "px"
+        fake.style.height = rect.height + "px"
+        
+        //Fonctions
+    
+            const onScroll = function() {
+                if (scrollY() > constraintBottom && element.style.position != "absolute") {
+                    element.style.position = "absolute"
+                    element.style.bottom = "0"
+                    element.style.top = "auto"
+                } else if (scrollY() > constraintBottom) {               
+                } else if (scrollY() > top - offset && element.style.position != "fixed") {
+                    element.classList.add('fixed')
+                    element.style.position = "fixed"
+                    element.style.top = offset + 'px'
+                    element.style.bottom = "auto"
+                    element.style.width = rect.width + 'px'
+                    element.parentNode.insertBefore(fake, element)
+                } else if (scrollY() < top - offset && element.style.position != "static") {
+                    element.classList.remove('fixed')
+                    element.style.position = "static"
+                    if (element.parentNode.contains(fake)) {
+                        element.parentNode.removeChild(fake)
+                    }
+                }
             }
-        }
+    
+            const onResize = () => {
+                element.style.width = "auto"
+                element.classList.remove('fixed')
+                element.style.position = "static"
+                fake.style.display = "none"
+                rect = element.getBoundingClientRect()
+                constraintRect = constraint.getBoundingClientRect()
+                constraintBottom = constraintRect.top + scrollY() + constraintRect.height - offset - rect.height
+                let top = rect.top + scrollY()
+                fake.style.width = rect.width + 'px'
+                fake.style.height = rect.height + 'px'
+                fake.style.display = "block"
+                onScroll()
+            }
+    
+            //Listeners
+            window.addEventListener('scroll', onScroll)
+            window.addEventListener('resize', onResize)
+    
+    }
 
-        const onResize = () => {
-            element.style.width = "auto"
-            element.classList.remove('fixed')
-            fake.style.display = "none"
-            rect = element.getBoundingClientRect()
-            let top = rect.top + scrollY()
-            fake.style.width = rect.width + 'px'
-            fake.style.height = rect.height + 'px'
-            fake.style.display = "block"
-            onScroll()
-        }
 
-        //Listeners
-        window.addEventListener('scroll', onScroll)
-        window.addEventListener('resize', onResize)
-
-    });
+    const elements = document.querySelectorAll('[data-fixed]')
+    elements.forEach(element => { makeStiky(element)});
 
 })()
